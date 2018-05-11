8762ec376814e729f2f4da97843ca09d0cb8ff13
TeqXe <1358582114@qq.com>
2018-05-11 17:55:39 +0800

    #task 更改启动主方法及pom.xml 文件，打成war包使其可以在外部tomcat中部署

--- src/main/java/top/yuyg/blog/BlogApplication.java
+++ src/main/java/top/yuyg/blog/BlogApplication.java
@@ -2,11 +2,21 @@ package top.yuyg.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+import top.yuyg.blog.datasources.DynamicDataSourceConfig;
 
 @SpringBootApplication
-public class BlogApplication {
+@Import({ DynamicDataSourceConfig.class })
+public class BlogApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogApplication.class);
 	}
 }
