79120f5350ca6984b264fde3b95405a88f56a407
TeqXe <1358582114@qq.com>
2018-05-18 00:50:42 +0800

    #bug thymeleaf集成还是存在问题，偶尔项目甚至无法访问

--- src/main/java/top/yuyg/blog/modules/sys/controller/ThymeleafController.java
+++ src/main/java/top/yuyg/blog/modules/sys/controller/ThymeleafController.java
@@ -2,12 +2,15 @@ package top.yuyg.blog.modules.sys.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ThymeleafController {
 
     @RequestMapping("admin/write")
-    public String write(){
-        return "/admin/writeblog";
+    public ModelAndView write(){
+        ModelAndView mav = new ModelAndView("admin/writeblog");
+        mav.addObject("hello", "你好啊,Thymeleaf");
+        return mav;
     }
 }
